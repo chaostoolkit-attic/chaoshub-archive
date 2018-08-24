f524123a381a213e2b82a6f100c1d033974f3e35
Sylvain Hellegouarch <sh@defuze.org>
2018-08-24 17:33:24 +0200

    Remove mock as not needed
    Signed-off-by: Sylvain Hellegouarch <sh@defuze.org>

--- app/tests/unit/dashboard/conftest.py
+++ app/tests/unit/dashboard/conftest.py
@@ -21,10 +21,8 @@ from chaoshubdashboard.settings import load_settings
 
 
 @pytest.fixture(scope="session")
-@patch('chaoshubdashboard.app.get_db_conn_uri_from_env', autospec=False)
-def app(get_db_conn_uri_from_env) -> Flask:
+def app() -> Flask:
     load_settings(os.path.join(os.path.dirname(__file__), "..", ".env.test"))
-    get_db_conn_uri_from_env.return_value = os.getenv("DB_HOST")
     application = create_app()
 
     with application.app_context():
